48a09617c3d8c0ac7d505e121749a1683edc2449
Rohit Pachunkar <Rohit.p@UK.MITL>
2020-06-30 08:26:26 +0530

    inital commit

---
+++ src/main/java/com/tcs/letmefly/LetmeflyApplication.java
@@ -0,0 +1,13 @@
+package com.tcs.letmefly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetmeflyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetmeflyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tcs/letmefly/LetmeflyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcs.letmefly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetmeflyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
